e8ff2ee22ca3c6f6bad211ae39e19e35452a56a1
Haley Visconti <hvisconti1343@outlook.com>
2025-04-11 22:44:09 +0100

    added delete endpoint

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func main() {
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodos)
 	app.Patch("/api/todos/:id", updateTodos)
-	// app.Delete("/api/todos", deleteTodos)
+	app.Delete("/api/todos/:id", deleteTodos)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -112,10 +112,28 @@ func updateTodos(c *fiber.Ctx) error {
 		return c.Status(400).JSON(fiber.Map{"error": "Invalid todo ID"})
 	}
 
-	filter := bson.M{"_id": objectID}
+	filter := bson.M{"_id":objectID}
 	update := bson.M{"$set": bson.M{"completed": true}}
 
 	_, err = collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"success": true})
+}
+
+func deleteTodos(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid ID"})
+	}
+
+	filter := bson.M{"_id":objectID}
+	_, err = collection.DeleteOne(context.Background(), filter)
+
 	if err != nil {
 		return err
 	}
